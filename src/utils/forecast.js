6e273b460efeb2f977152b84a5a793caf02c4fe2
Wimdad <official.waquar@gmail.com>
2020-11-14 19:00:57 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=18bff234db9331212dd016a7a49a0c6a&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+        }
+    })
+}
+
+module.exports = forecast
